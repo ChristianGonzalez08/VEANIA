e2eba8ee26c59b2e882793b21e30167bdaa1d4d3
Christian J. Gonzalez Anaya <chris.gon.an08@gmail.com>
2022-11-15 00:39:13 -0600

    agregar modelo Ordenes de Compra

---
+++ app/Http/Controllers/OrdenesCompraController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrdenesCompra;
+use Illuminate\Http\Request;
+
+class OrdenesCompraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OrdenesCompra  $ordenesCompra
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrdenesCompra $ordenesCompra)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OrdenesCompra  $ordenesCompra
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OrdenesCompra $ordenesCompra)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OrdenesCompra  $ordenesCompra
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrdenesCompra $ordenesCompra)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OrdenesCompra  $ordenesCompra
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrdenesCompra $ordenesCompra)
+    {
+        //
+    }
+}
