f125ca6081df0dff83db925f67773da1f18d58f4
Christian J. Gonzalez Anaya <chris.gon.an08@gmail.com>
2022-11-16 00:16:29 -0600

    Agregando columnas para tabla de salidas

--- database/migrations/2022_11_15_064838_create_salidas_table.php
+++ database/migrations/2022_11_15_064838_create_salidas_table.php
@@ -15,8 +15,64 @@ return new class extends Migration
     {
         Schema::create('salidas', function (Blueprint $table) {
             $table->id();
-            $table->string("tipo");
-            $table->string("NoParte");
+            $table->string("IDcomprb");
+
+            //Datos Producto
+            $table->string("CodProduct");
+            $table->string("Nombre");
+            $table->string("Familia");
+            $table->string("Sub-Familia");
+
+            //Informacion de Salida
+            $table->string("TipoSalida");
+            $table->string("FechaSalida");
+            $table->string("ComprbInteggra");
+            $table->string("FechaCmprbInteggra");
+
+            //Detalles Venta
+            $table->string("CantidadSalida");
+            $table->string("PrecioVenta");
+            $table->string("PrecioRegular");
+
+            $table->string("CostoUnitario");
+            $table->string("PrecioPromocion");
+            $table->string("PorcentajeUtilidad");
+            $table->string("Utilidad");
+            $table->string("Moneda");
+
+            //Datos Vendedor
+            $table->string("Vendedor");
+            $table->string("comision");
+            $table->string("EstatusComision");
+
+            //Datos Cliente
+            $table->string("Cliente");
+            $table->string("NoCliente");
+
+            //Datos Entrega
+            $table->string("FechaEntrega");
+            $table->string("ResponEntrega");
+            $table->string("ResponRecepcion");
+            $table->string("Firma/Sello");
+
+
+            //Datos del Pago
+            $table->string("FormaPago");
+            $table->string("NoPago");
+
+
+            $table->string("OC");
+            $table->string("Presupesto");
+            $table->string("NombrePresupuesto");
+            $table->string("ResponsableMovimiento");
+            $table->string("SucSalida");
+
+            //devoluciones
+            $table->string("FechaDevolucion");
+            $table->string("CausaDevolucion");
+
+
+
             $table->timestamps();
 
         });
